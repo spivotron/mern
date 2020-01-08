81e3e11fa67c97ad621ae7a6db5799c877b6b367
Henry Spivey <spivotron@gmail.com>
2020-01-08 15:26:54 -0800

    initial commit

---
+++ src/build/precache-manifest.f16b099222c17d0669924fe960d911af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c757ea09544e303f3e2acdcd90f1e8e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2998897c4f6a1be4af17",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "da2ed9b6533f4aaa63aa",
+    "url": "/static/js/2.456433e0.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.456433e0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2998897c4f6a1be4af17",
+    "url": "/static/js/main.decba616.chunk.js"
+  },
+  {
+    "revision": "6e55ea343e46b8691e3d",
+    "url": "/static/js/runtime-main.b5470c2c.js"
+  }
+]);
